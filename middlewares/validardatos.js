2ea3e6654cbfcc94df6db2367a840b6351ce7a53
Diego <diegoborregales7@gmail.com>
2025-04-13 13:31:46 -0400

    listo por hoy

--- middlewares/validardatos.js
+++ middlewares/validardatos.js
@@ -3,7 +3,10 @@ const clienteController = require('../controllers/clienteController'); // Contro
 const deudaController = require('../controllers/deudacontroller'); // Controlador de deudas
 const pagoController = require('../controllers/pagoController'); // Controlador de pagos
 
-
+function llamada(){
+    deudaController.leerDeudas();
+    pagoController.leerPagos();
+}
 
 const deudas = deudaController.leerDeudas();
 const verPagos = pagoController.leerPagos();
@@ -50,60 +53,69 @@ function validartodo (nuevoPago){
 
 
 
+async function calcularDeudaTotal(userPagador) {
+    const userR = userPagador;
+    const deudasv = deudas.filter((deuda) => deuda.nombre === userR);
+    let deudatotal = 0; // Reiniciar deudatotal a 0
+    console.log(deudasv);
 
-async function calcularDeudaTotal(userPagador){
-    const userR = userPagador
-    const deudasv = deudas.filter((deuda) => deuda.nombre === userR)
-    console.log(deudasv)
-    
-    if(deudasv){
-        deudasv.forEach((deuda) =>{
+    if (deudasv) {
+        deudasv.forEach((deuda) => {
             deudatotal += Number(deuda.deuda);
-        })
-        //deudatotal += Number(deudas.deuda);
+        });
     }
-    return console.log('desde aqui  '+ deudatotal);
+    console.log('deuda  ' + deudatotal);
+    return deudatotal; // Retornar el valor calculado
 }
 
+async function calcularPagostotal(userPagador) {
+    const userR = userPagador;
+    const pagosv = verPagos.filter((pago) => pago.nombre === userR);
+    let pagototal = 0; // Reiniciar pagototal a 0
+    console.log(pagosv);
 
-async function calcularPagostotal(userPagador){
-    const userR = userPagador
-    const pagosv = verPagos.filter((pago) => pago.nombre === userR)
-    console.log(pagosv)
-    
-    if(pagosv){
-        pagosv.forEach((pago) =>{
+    if (pagosv) {
+        pagosv.forEach((pago) => {
             pagototal += Number(pago.pago);
-        })
-        //deudatotal += Number(deudas.deuda);
+        });
     }
-    return console.log('pagos:  '+ pagototal);
+    console.log('pagos:  ' + pagototal);
+    return pagototal; // Retornar el valor calculado
 }
 
+async function validarpagomonto(pagouser, nombreuser, nuevoPago) {
+    try {
+        const deudaPendiente = await calcularDeudaPendiente(nombreuser);
+        verificarMonto(pagouser, deudaPendiente);
+    } catch (error) {
+        console.error('Error en validarpagomonto:', error);
+        mensajeA.push('Error al validar el pago');
+    }
 
+    async function calcularDeudaPendiente(nombreuser) {
+        const deudaTotal = await calcularDeudaTotal(nombreuser);
+        const pagoTotal = await calcularPagostotal(nombreuser);
+        return deudaTotal - pagoTotal;
+    }
 
+    function verificarMonto(pagouser, deudaPendiente) {
+        const pago = Number(pagouser);
+        console.log('Deuda pendiente:', deudaPendiente, 'Pago:', pago);
 
-//calcularDeudaTotal(diego)
-async function validarpagomonto( pagouser , nombreuser, nuevoPago){
-    calcularPagostotal(nombreuser)
-    calcularDeudaTotal(nombreuser)
-    Number(pagouser)
-    Number(pagototal);
-    const x = deudatotal - pagototal;
-    console.log(x)
-    if(pagouser > x){
-        
-        return mensajeA.push('El monto del pago no puede ser mayor que la deuda pendiente. Por favor, verifica el monto ingresado y realiza el pago correcto. Si crees que hay un error en el sistema, no dudes en contactar al contador o al administrador web.');
-        
-    }else{
-        guardardeuda(nuevoPago)
-        
-        return mensajeA.push('todo bien')
-       
+        if (pago > deudaPendiente) {
+            return mensajeA.push('El monto del pago no puede ser mayor que la deuda pendiente. Por favor, verifica el monto ingresado y realiza el pago correcto. Si crees que hay un error en el sistema, no dudes en contactar al contador o al administrador web.');
+        } else {
+            guardardeuda(nuevoPago);
+           
+            // Recalcular la deuda pendiente después de guardar el pago
+            calcularDeudaPendiente(nombreuser).then(nuevaDeudaPendiente => {
+                console.log('Nueva deuda pendiente:', nuevaDeudaPendiente);
+                return mensajeA.push('todo bien');
+            });
+        }
     }
 }
-
-
+llamada()
 
 
 module.exports={
